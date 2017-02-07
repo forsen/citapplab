62f8785637ff7612823cac2b4b31cf2adc31c9b2
Erik Haider Forsén <erikhf@ifi.uio.no>
2017-02-07 21:56:11 +0100

    Not working, will find out later

--- src/index.js
+++ src/index.js
@@ -1,22 +1,8 @@
-export const welcome = () => {
-  const ENVIRONMENT_IS_REACT_NATIVE = typeof navigator === 'object' && navigator.product === 'ReactNative'
-  const ENVIRONMENT_IS_NODE = typeof process === 'object' && typeof require === 'function'
-  const ENVIRONMENT_IS_WEB = typeof window === 'object'
-  const ENVIRONMENT_IS_WORKER = typeof importScripts === 'function'
 
-  if (ENVIRONMENT_IS_REACT_NATIVE) {
-    console.log('reactnative')
-    return 'reactnative'
-  } else if (ENVIRONMENT_IS_WORKER) {
-    console.log('worker')
-    return 'worker'
-  } else if (ENVIRONMENT_IS_WEB) {
-    console.log('web')
-    return 'web'
-  } else if (ENVIRONMENT_IS_NODE) {
-    console.log('node')
-    return 'node'
-  } else {
-    throw new Error('Unsupported environment for fetch-intercept')
-  }
+export const verify = () => {
+  console.log(fetch('http://google.no')
+    .then((result) => result.json())
+    .then((result) => console.log(result)))
+  return fetch !== undefined
 }
+
--- test/index.spec.js
+++ test/index.spec.js
@@ -1,12 +1,12 @@
 import chai from 'chai'
-import { welcome } from '../src'
+import { verify } from '../lib/bundle'
 
 chai.expect()
 
 const expect = chai.expect
 
-describe('Hello world', function () {
-  it('welcome should be "hello world"', () => {
-    expect(welcome()).to.be.equal('node')
+describe('Initial tests', function () {
+  it('fetch API (or polyfill) should be available', () => {
+    expect(verify()).to.be.true
   })
 })
