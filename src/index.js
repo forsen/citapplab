8ae0d222d122a28492c5104de4d4e01e337674e7
Erik Haider Forsén <erikhf@ifi.uio.no>
2017-02-07 13:43:54 +0100

    Checking which environment is in use

--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
-export default class Citapplab {
-  constructor () {
-    this._welcome = 'Hello World'
-  }
+export const welcome = () => {
+  const ENVIRONMENT_IS_REACT_NATIVE = typeof navigator === 'object' && navigator.product === 'ReactNative'
+  const ENVIRONMENT_IS_NODE = typeof process === 'object' && typeof require === 'function'
+  const ENVIRONMENT_IS_WEB = typeof window === 'object'
+  const ENVIRONMENT_IS_WORKER = typeof importScripts === 'function'
 
-  get welcome () {
-    return this._welcome
+  if (ENVIRONMENT_IS_REACT_NATIVE) {
+    console.log('reactnative')
+    return 'reactnative'
+  } else if (ENVIRONMENT_IS_WORKER) {
+    console.log('worker')
+    return 'worker'
+  } else if (ENVIRONMENT_IS_WEB) {
+    console.log('web')
+    return 'web'
+  } else if (ENVIRONMENT_IS_NODE) {
+    console.log('node')
+    return 'node'
+  } else {
+    throw new Error('Unsupported environment for fetch-intercept')
   }
 }
--- test/index.spec.js
+++ test/index.spec.js
@@ -1,18 +1,12 @@
 import chai from 'chai'
-import Citapplab from '../src'
+import { welcome } from '../src'
 
 chai.expect()
 
 const expect = chai.expect
 
-let lib
-
-before(function () {
-  lib = new Citapplab()
-})
-
 describe('Hello world', function () {
   it('welcome should be "hello world"', () => {
-    expect(lib.welcome).to.be.equal('Hello World')
+    expect(welcome()).to.be.equal('node')
   })
 })
